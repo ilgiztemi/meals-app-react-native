f8932936f5ac00f53fa6d895997bfcb3167fc33e
Kanykey Albanova <temirbekov91@gmail.com>
2022-12-14 23:53:59 -0500

    added header buttons

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,21 @@
-import { Text, Image, View, StyleSheet, ScrollView } from "react-native";
+import { useLayoutEffect } from "react";
+import { Text, Image, View, StyleSheet, ScrollView, Button } from "react-native";
 import List from "../components/MealDetail/LIst";
 import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
 
-const MealDetailScreen = ( { route } ) => {
+const MealDetailScreen = ( { route, navigation } ) => {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find( el => el.id === mealId );
+  const headerButtonPressHandler = () => {
+    console.log("pressed")
+  }
+  useLayoutEffect(() =>{
+    navigation.setOptions({
+      headerRight: () => <Button title="Tap me" onPress={headerButtonPressHandler} />
+    })
+  }, [navigation, headerButtonPressHandler])
   return (
     <ScrollView style={styles.rootContainer}>
       <Image style={ styles.image } source={ { uri: selectedMeal.imageUrl } } />
