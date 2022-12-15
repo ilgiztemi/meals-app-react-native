a4b2105069db0197350186f9264671645cefc584
Kanykey Albanova <temirbekov91@gmail.com>
2022-12-14 21:03:12 -0500

    added and configured the meal detail screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
           //   }
           // }} 
           />
+          <Stack.Screen name="MealDetail" component={ MealDetailScreen } />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+const MealDetailScreen = ( { route } ) => {
+  const mealId = route.params.mealId;
+  return (
+    <Text>{ mealId }</Text>
+  );
+};
+
+export default MealDetailScreen;
