b1f900626a1e271b82677871d706286ce517a505
Kanykey Albanova <temirbekov91@gmail.com>
2023-07-02 19:43:19 -0400

    added scheduling notification

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import * as Notifications from 'expo-notifications'
 
 const FavoritesScreen = () => {
+  const scheduleNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the body of the notification",
+        data: {
+          userName: 'Max'
+        }
+      },
+      trigger: {
+        seconds: 5
+      }
+    });
+  };
   return (
-    <View>
-      <Text>The favorites screen!</Text>
+    <View style={ styles.container }>
+      <Text style={ styles.text }>The favorites screen!</Text>
+      <Button style={ ( { pressed } ) => [ pressed && styles.pressed ] } title="Schedule Notifications" onPress={ scheduleNotificationHandler } />
     </View>
   );
 };
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create( {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    marginBottom: 16
+  },
+  pressed: {
+    opacity: 0.7
+  }
+} );
